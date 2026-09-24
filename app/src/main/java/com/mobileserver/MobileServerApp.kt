6b01043ef1a54268ab293afc1978cfc3bcdd828d
dev <dev@local>
2026-09-24 19:37:32 +0800

    feat: 加全局异常捕获,崩溃日志写入crash.log

--- app/src/main/java/com/mobileserver/MobileServerApp.kt
+++ app/src/main/java/com/mobileserver/MobileServerApp.kt
@@ -1,11 +1,25 @@
 package com.mobileserver
 
 import android.app.Application
+import android.util.Log
 import com.mobileserver.core.Paths
+import com.mobileserver.util.LogManager
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 
 class MobileServerApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Paths.init(this)
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            val sw = StringWriter()
+            e.printStackTrace(PrintWriter(sw))
+            try {
+                LogManager.append("crash", "[crash] ${t.name}: $sw")
+            } catch (_: Exception) {}
+            Log.e("MobileServer", "CRASH", e)
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
     }
 }
